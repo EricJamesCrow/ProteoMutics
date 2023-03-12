532d93dfe12aabda315a3b75b6d03de56fbf7d0d
cameroncordero <cameroncordero@gmail.com>
2023-03-12 15:55:55 -0700

    changes from my pre-processing

--- backend/python/Controller.py
+++ backend/python/Controller.py
@@ -17,13 +17,16 @@ def check_if_pre_processed(file_path: Path, typ: str):
 
 def pre_process_mutation_file(file_path: Path, fasta_file: Path):
     directory = file_path.parent
-    folder_name = directory / file_path.stem+'_Nucleomutics'
+    nucleomutics_folder = directory / file_path.stem+'_Nucleomutics'
     temp_folder = directory / '.intermediate_files'
-    folder_name.mkdir()
+    nucleomutics_folder.mkdir()
     temp_folder.mkdir()
     step_1 = PreProcessing.vcf_snp_to_intermediate_bed(file_path, temp_folder)
     step_2 = PreProcessing.expand_context_custom_bed(step_1, fasta_file, temp_folder)
-    step_3 = 
+    step_3 = PreProcessing.filter_acceptable_chromosomes(step_2, temp_folder)
+    _, step_4 = PreProcessing.check_and_sort(step_3, nucleomutics_folder, '.mut')
+    temp_folder.rmdir()
+    return step_4
 
 def pre_process_nuc_map():
     pass
