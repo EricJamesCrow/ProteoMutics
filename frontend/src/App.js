c4edb349ee2685065473add00386ea4bd98aeb03
EricJamesCrow <EricCrow@pm.me>
2023-03-11 20:59:41 -0800

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
+// styles
+import './App.css'
+
 // components
 import TitleBar from './components/TitleBar'
+
+// images
+import Resize from './images/resize_icon.svg'
 
 export default function App() {
   return (
     <>
     <TitleBar/>
+    <Resize className="footer"/>
     </>
   )
 }
