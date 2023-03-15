4adcebe1a16cbb15326d8465e52556f62c5f53db
EricJamesCrow <EricCrow@pm.me>
2023-03-14 19:25:40 -0700

    added splash loading screen

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,26 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const { dialog } = require('electron');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
 let mainWindow;
+let splashWindow; // Added splashWindow variable
+
+const createSplashWindow = () => { // Added createSplashWindow function
+  splashWindow = new BrowserWindow({
+    width: 350,
+    height: 400,
+    frame: false,
+    alwaysOnTop: true,
+    resizable: false,
+  });
+
+  splashWindow.loadFile(path.join(__dirname, '../../src/splash.html'));
+};
 
 const createWindow = () => {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1440,
     height: 810,
@@ -18,29 +29,30 @@ const createWindow = () => {
     minHeight: 640,
     frame: false,
     autoHideMenuBar: true,
+    show: false, // Added show: false to hide the main window initially
     webPreferences: {
       preload: path.resolve(__dirname, '../../src/preload.js'),
-      nodeIntegration: false, // is default value after Electron v5
-      contextIsolation: true, // protect against prototype pollution
-      enableRemoteModule: false, // turn off remote
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  mainWindow.once('ready-to-show', () => { // Added event listener to close splash and show main window
+    splashWindow.close();
+    mainWindow.show();
+  });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createSplashWindow(); // Added createSplashWindow call
+  createWindow();
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -48,15 +60,11 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
 ipcMain.handle('minimize-window', () => {
   mainWindow.minimize();
 });
@@ -85,4 +93,3 @@ ipcMain.handle('show-file-dialog', async (event, allowedFileTypes) => {
   });
   return result.filePaths[0];
 });
-
