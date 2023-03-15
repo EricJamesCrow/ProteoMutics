07195c74454bf55a74b2bd6b8b3777e4d8d2dfff
EricJamesCrow <EricCrow@pm.me>
2023-03-15 12:08:08 -0700

    created Statistics component

--- frontend/src/pages/visualization/VisualizationContainer.js
+++ frontend/src/pages/visualization/VisualizationContainer.js
@@ -5,6 +5,7 @@ import './VisualizationContainer.css'
 
 // components
 import GraphComponent from './components/GraphComponent'
+import StatisticsComponent from './components/StatisticsComponent'
 
 export default function VisualizationContainer() {
 
@@ -13,6 +14,7 @@ export default function VisualizationContainer() {
       <div className="display-graphs-container">
         <GraphComponent />
       </div>
+      <StatisticsComponent />
       <div className="visualization-btns-container">
         <button className="preview-graph-btn">Preview</button>
         <button className="save-graph-btn">Save</button>
---
+++ frontend/src/pages/visualization/components/StatisticsComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function StatisticsComponent() {
+  return (
+    <div className="statistics-component-container">
+        <div className="data-formatting-with-switch">
+            <h1>Statistics</h1>
+        </div>
+        <ul>
+            <li>Periodicity</li>
+            <li>Signal to Noise ratio</li>
+            <li>Confidence</li>
+        </ul>
+    </div>
+  )
+}
