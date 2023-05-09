35778d4d36f28c8a2625bcac26c3fc53b963cb08
cameroncordero <cameroncordero@gmail.com>
2023-05-08 21:10:51 -0700

    im dumb

--- backend/python/Controller.py
+++ backend/python/Controller.py
@@ -52,7 +52,7 @@ def pre_process_nucleosome_map(file_path: Path, fasta_file: Path):
     temp_folder.mkdir()
     step_1 = PreProcessing.adjust_dyad_positions(file_path, temp_folder)
     step_2 = BedtoolsCommands.bedtools_getfasta(step_1, fasta_file)
-    step_3, fasta = PreProcessing.filter_lines_with_n(Path(step_2), file_path, temp_folder)
+    step_3, fasta = PreProcessing.filter_lines_with_n(Path(step_2[1]), file_path, temp_folder)
     step_4 = PreProcessing.filter_acceptable_chromosomes(step_3, temp_folder)
     new_dyad = PreProcessing.check_and_sort(step_4, nucleomutics_folder, '.nuc')
     counts = DyadContextCounter(fasta, nucleomutics_folder)
