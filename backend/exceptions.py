442d53e65ac02f6cba0bde2016c0c2e994643819
EricJamesCrow <EricCrow@pm.me>
2023-10-01 09:30:53 -0700

    removed rest of django files

--- backend/exceptions.py
+++
@@ -1,7 +0,0 @@
-class FileNotProcessedError(Exception):
-    """Raised when the file hasn't been processed."""
-    pass
-
-class PreProcessingError(Exception):
-    """Raised during any pre-processing related errors."""
-    pass
